486baf242f2f70169dca49c136c85c788de1af7d
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2024-12-19 22:05:17 -0500

    Begin defining NumberInput class

---
+++ src/components/NumberInput.ts
@@ -0,0 +1,82 @@
+import Fraction from "fraction.js";
+
+/**
+ * Handles element visibility and input validation for user input of numbers
+ */
+export class NumberInput {
+  constructor(
+    private kindSelect: HTMLSelectElement,
+    private integerInput: HTMLInputElement,
+    private numeratorInput: HTMLInputElement,
+    private denominatorInput: HTMLInputElement,
+  ) {
+    this.kindSelect.addEventListener<'change'>('change', this.responsiveInputVisibility.bind(this));
+    this.integerInput.addEventListener<'input'>('input', this.enforceNumericInput);
+    this.numeratorInput.addEventListener<'input'>('input', this.enforceNumericInput);
+    this.denominatorInput.addEventListener<'input'>('input', this.enforceNumericInput);
+  }
+
+  /**
+   * Remove any non-numeric characters from an input element as they are entered
+   * @param event - the input event that triggers the method
+   */
+  private enforceNumericInput(event: Event): void {
+    const target = event.currentTarget as HTMLInputElement;
+    target.value = target.value.replace(/[^\d]/g, '');
+  }
+
+  /**
+   * Clear the value of all input fields and set their visibility based on the 
+   *    user's selected type of number. 
+   * 
+   * `display` for the input elements should not be set with CSS so that 
+   *    `hidden` can be set to true or false here.
+   */
+  private responsiveInputVisibility(): void {
+    this.integerInput.value = '';
+    this.numeratorInput.value = '';
+    this.denominatorInput.value = '';
+    switch (this.kindSelect.value) {
+      case 'fraction':
+        this.integerInput.hidden = true;
+        this.numeratorInput.hidden = false;
+        this.denominatorInput.hidden = false;
+        break;
+      case 'mixed-number':
+        this.integerInput.hidden = false;
+        this.numeratorInput.hidden = false;
+        this.denominatorInput.hidden = false;
+        break;
+      case 'integer':
+        this.integerInput.hidden = false;
+        this.numeratorInput.hidden = true;
+        this.denominatorInput.hidden = true;
+        break;
+      default:
+        return;
+    }
+  }
+
+  /**
+   * Create a Fraction object from the user's inputs. Default values of 0 0/1 (integer=0, 
+   *    numerator=0, denominator=0) are provided for cases when not all of the fields are visible 
+   *    and used. 
+   * 
+   * @returns the Fraction object
+   */
+  public getFraction(): Fraction {
+    const integerPart: number = this.integerInput.value 
+                                    ? parseInt(this.integerInput.value) 
+                                    : 0;
+    const numeratorPart: number = this.numeratorInput.value 
+                                      ? parseInt(this.numeratorInput.value) 
+                                      : 0;
+    const denominatorPart: number = this.denominatorInput.value 
+                                      ? parseInt(this.denominatorInput.value) 
+                                      : 1;
+    const ret = new Fraction((integerPart * denominatorPart + numeratorPart), denominatorPart);
+    return ret;
+  }
+
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,59 @@
 import Fraction from "fraction.js";
 
-const firstKind = document.getElementById('first-kind') as HTMLSelectElement;
-const firstIntegerInput = document.getElementById('first-integer') as HTMLInputElement;
-const firstNumeratorInput = document.getElementById('first-numerator') as HTMLInputElement;
-const firstDenominatorInput = document.getElementById('first-denominator') as HTMLInputElement;
+import { NumberInput } from "./components/NumberInput";
 
-const secondKind = document.getElementById('second-kind') as HTMLSelectElement;
-const secondIntegerInput = document.getElementById('second-integer') as HTMLInputElement;
-const secondNumeratorInput = document.getElementById('second-numerator') as HTMLInputElement;
-const secondDenominatorInput = document.getElementById('second-denominator') as HTMLInputElement;
+const firstNumber = new NumberInput(
+  document.getElementById('first-kind') as HTMLSelectElement,
+  document.getElementById('first-integer') as HTMLInputElement,
+  document.getElementById('first-numerator') as HTMLInputElement,
+  document.getElementById('first-denominator') as HTMLInputElement,
+);
+
+const secondNumber = new NumberInput(
+  document.getElementById('second-kind') as HTMLSelectElement,
+  document.getElementById('second-integer') as HTMLInputElement,
+  document.getElementById('second-numerator') as HTMLInputElement,
+  document.getElementById('second-denominator') as HTMLInputElement,
+);
+
+
+
 
 const calculateBtn = document.getElementById('calculate-btn') as HTMLButtonElement;
 
 
-firstIntegerInput.addEventListener('input', enforceNumericInput);
-firstNumeratorInput.addEventListener('input', enforceNumericInput);
-firstDenominatorInput.addEventListener('input', enforceNumericInput);
-
-secondIntegerInput.addEventListener('input', enforceNumericInput);
-secondNumeratorInput.addEventListener('input', enforceNumericInput);
-secondDenominatorInput.addEventListener('input', enforceNumericInput);
-
-firstKind.addEventListener('change', (event: Event) => {
-  const target = event.currentTarget as HTMLSelectElement;
-  responsiveInputVisibility.call(target, firstIntegerInput, firstNumeratorInput, firstDenominatorInput);
-});
-
-secondKind.addEventListener('change', (event: Event) => {
-  const target = event.currentTarget as HTMLSelectElement;
-  responsiveInputVisibility.call(target, secondIntegerInput, secondNumeratorInput, secondDenominatorInput);
-});
-
 calculateBtn.addEventListener('click', (): void => {
 
-  // Avoid division by 0
-  if (firstDenominatorInput.value === '0') {
-    alert('Denominator cannot be 0');
-    return;
-  }
 
-  // Get numeric values for the first number or use default values if blank
-  const firstInteger: number = firstIntegerInput.value 
-                                 ? parseInt(firstIntegerInput.value) 
-                                 : 0;
-  const firstNumerator: number = firstNumeratorInput.value 
-                                   ? parseInt(firstNumeratorInput.value) 
-                                   : 0;
-  const firstDenominator: number = firstDenominatorInput.value 
-                                   ? parseInt(firstDenominatorInput.value) 
-                                   : 1;
+  console.log(firstNumber.getFraction().toFraction(true));
 
-  // Convert user's values to a Fraction object
-  const firstNumber: Fraction = new Fraction(
-    (firstInteger * firstDenominator + firstNumerator), 
-    firstDenominator,
-  );
+  // // Avoid division by 0
+  // if (firstDenominatorInput.value === '0') {
+  //   alert('Denominator cannot be 0');
+  //   return;
+  // }
 
-  console.log(firstNumber.toFraction(true));
+  // // Get numeric values for the first number or use default values if blank
+  // const firstInteger: number = firstIntegerInput.value 
+  //                                ? parseInt(firstIntegerInput.value) 
+  //                                : 0;
+  // const firstNumerator: number = firstNumeratorInput.value 
+  //                                  ? parseInt(firstNumeratorInput.value) 
+  //                                  : 0;
+  // const firstDenominator: number = firstDenominatorInput.value 
+  //                                  ? parseInt(firstDenominatorInput.value) 
+  //                                  : 1;
+
+  // // Convert user's values to a Fraction object
+  // const firstNumber: Fraction = new Fraction(
+  //   (firstInteger * firstDenominator + firstNumerator), 
+  //   firstDenominator,
+  // );
+
+  // console.log(firstNumber.toFraction(true));
 
 
 
 
 
 });
-
-/**
- * Remove any non-numeric characters from an input element as they are entered
- * @param this - the input element that the function will act upon
- */
-function enforceNumericInput(this: HTMLInputElement): void {
-  this.value = this.value.replace(/[^0-9]/g, '');
-}
-
-/**
- * Set the visibility of input fields based on the user's selection of Fraction, 
- *    Mixed Number, or Integer, clearing the value of all fields on change. 
- *    `display` for these elements should not be set with CSS so that `hidden` 
- *    can be set to true or false here.
- * 
- * @param this - the select element where the user chooses the type of number
- * @param integer - the input element for the integer part of the number
- * @param numerator - the input element for the numerator part of the number
- * @param denominator - the input element for the denominator part of the number
- */
-function responsiveInputVisibility(
-  this: HTMLSelectElement, 
-  integer: HTMLInputElement, 
-  numerator: HTMLInputElement, 
-  denominator: HTMLInputElement,
-): void {
-  integer.value = '';
-  numerator.value = '';
-  denominator.value = '';
-  switch (this.value) {
-    case 'fraction':
-      integer.hidden = true;
-      numerator.hidden = false;
-      denominator.hidden = false;
-      break;
-    case 'mixed-number':
-      integer.hidden = false;
-      numerator.hidden = false;
-      denominator.hidden = false;
-      break;
-    case 'integer':
-      integer.hidden = false;
-      numerator.hidden = true;
-      denominator.hidden = true;
-      break;
-    default:
-      return;
-  }
-}
