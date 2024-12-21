fd205851bcfee90d5b29c8024be00075dbb06018
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2024-12-21 17:58:46 -0500

    Set up debugging and jest.setup.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,11 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '^@components/(.*)$': '<rootDir>/src/components/$1',
-    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
-  },
+  // moduleNameMapper: {
+  //   '^@components/(.*)$': '<rootDir>/src/components/$1',
+  //   '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+  // },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 export default config;
--- src/utils/Utils.test.ts
+++ src/utils/Utils.test.ts
@@ -1,9 +1,33 @@
 import { Utils } from "./Utils";
 
-describe('Utils', () => {
-  test('getValidatedElement returns a valid element', () => {
-    document.body.innerHTML = '<h1 id="title">This is my title.<h1>';
-    const title = Utils.getValidatedElement('title', HTMLHeadingElement);
-    expect(title.textContent).toBe('This is my title.');
+describe('getValidatedElement', () => {
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <h1 id="title">This is my title.</h1>
+      <input id="input-field">
+    `;
   });
+  
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('returns a valid element', () => {
+    const title = Utils.getValidatedElement('title', HTMLHeadingElement);
+    const input = Utils.getValidatedElement('input-field', HTMLInputElement);
+    expect(title.textContent).toBe('This is my title.');
+    expect(input).toBeInTheDocument();
+  });
+
+  test("throws an error for an element that doesn't exist", () => {
+    expect(Utils.getValidatedElement('not-here', HTMLParagraphElement))
+      .toThrow('Element with ID "not-here" not found');
+  });
+
+  test('throws an error for an element of the incorrect type', () => {
+    expect(Utils.getValidatedElement('input-field', HTMLSelectElement))
+      .toThrow('Element with ID "input-field" is not of type HTMLSelectElement');
+  });
+
 });
