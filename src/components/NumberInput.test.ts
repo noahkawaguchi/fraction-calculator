4ddf29e3de5ef3500465a94fc5e3939eedca26b2
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2024-12-21 22:06:38 -0500

    Start writing tests for NumberInput

--- src/components/NumberInput.test.ts
+++ src/components/NumberInput.test.ts
@@ -1,3 +1,43 @@
-test('dummy test', () => {
-  expect(1).toBe(1);
+import { screen } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
+
+import { NumberInput } from "./NumberInput";
+
+describe('NumberInput', () => {
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <p id="negative-sign" hidden>—</p>
+      <input id="integer-input" type="number" value="0" min="0" hidden>
+      <input id="numerator-input" type="number" value="0" min="0">
+      <input id="denominator-input" type="number" value="1" min="1">
+      <select name="sign-select" id="sign-select">
+        <option selected>Positive</option>
+        <option>Negative</option>
+      </select>
+      <select name="kind-select" id="kind-select">
+        <option selected>Fraction</option>
+        <option>Mixed Number</option>
+        <option>Integer</option>
+      </select>
+    `;
+
+    const ni = new NumberInput({
+      negativeSignId: 'negative-sign',
+      integerInputId: 'integer-input',
+      numeratorInputId: 'numerator-input',
+      denominatorInputId: 'denominator-input',
+      signSelectId: 'sign-select',
+      kindSelectId: 'kind-select',
+    });
+
+  });
+
+  it('should show and hide the negative sign in response to user selection', async () => {
+    const negativeSign: HTMLParagraphElement = screen.getByText('—');
+    // const signSelect: HTMLSelectElement = screen.getByRole('combobox'); // There's more than one - labels necessary
+  });
+
+  // TODO: use async/await for userEvent tests
+
 });
--- src/utils/Utils.test.ts
+++ src/utils/Utils.test.ts
@@ -1,6 +1,6 @@
-import { Utils } from "./Utils";
-
 import { screen } from '@testing-library/dom'
+
+import { Utils } from "./Utils";
 
 describe('getValidatedElement', () => {
 
@@ -11,19 +11,19 @@ describe('getValidatedElement', () => {
     `;
   });
 
-  test('returns a valid element', () => {
+  it('should retrieve and return the element if valid', () => {
     const title = Utils.getValidatedElement('title', HTMLHeadingElement);
     const input = Utils.getValidatedElement('input-field', HTMLInputElement);
     expect(title).toHaveTextContent('This is my title.');
     expect(input).toBeInTheDocument();
   });
 
-  test("throws an error for an element that doesn't exist", () => {
+  it("should throw an error for an element that doesn't exist", () => {
     expect(() => Utils.getValidatedElement('not-here', HTMLParagraphElement))
       .toThrow('Element with ID "not-here" not found');
   });
 
-  test('throws an error for an element of the incorrect type', () => {
+  it('should throw an error for an element of the incorrect type', () => {
     expect(() => Utils.getValidatedElement('input-field', HTMLSelectElement))
       .toThrow('Element with ID "input-field" is not of type HTMLSelectElement');
   });
@@ -32,7 +32,7 @@ describe('getValidatedElement', () => {
 
 /** 
  * It is difficult to test this method in much detail without manual visual 
- * inspection due to the intricate HTML and CSS that KaTeX generates
+ * inspection due to the intricate HTML and CSS that KaTeX generates.
  */
 describe('renderAllTex', () => {
 
@@ -40,16 +40,16 @@ describe('renderAllTex', () => {
     document.body.innerHTML = '<p id="tex">no TeX here yet</p>';
   });
   
-  test('renders inline math', () => {
-    const tex = screen.getByText('no TeX here yet');
+  it('should render inline math', () => {
+    const tex: HTMLParagraphElement = screen.getByText('no TeX here yet');
     tex.textContent = '\\( \\frac{3}{4} \\)';
     Utils.renderAllTex();
     const inline = document.querySelector('.katex');
     expect(inline).toBeInTheDocument();
   });
 
-  test('renders display math', () => {
-    const tex = screen.getByText('no TeX here yet');
+  it('should render display math', () => {
+    const tex: HTMLParagraphElement = screen.getByText('no TeX here yet');
     tex.textContent = '$$\\frac{1}{2}$$';
     Utils.renderAllTex();
     const display = document.querySelector('.katex-display');
